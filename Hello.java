3c6111a14dade43238e926052325eae789b4df04
jaseelan <93515798+jaseelan@users.noreply.github.com>
2021-12-14 13:51:54 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,49 @@
+package simpleHello;
+
+import java.awt.Point;
+import java.util.Date;
+
+public class Hello {
+
+	public static void main(String[] args) {
+//		byte age = 32;
+//		long viewsCount = 3_123_456_789L;
+//		float price = 10.99F;
+//		char letter = 'j';
+//		boolean isEligible = false;
+//		System.out.println(age);
+//		System.out.println(viewsCount);
+//		System.out.println(price);
+//		System.out.println(letter);
+//		System.out.println(isEligible);
+//
+//		byte age = 33;
+//		Date now = new Date();
+//		System.out.println(age);
+//		System.out.println("time: " + now);
+
+//		byte x = 1;  //primitive  not effect tha value
+//		byte y = x;
+//		x = 2;
+//		System.out.println(y);
+
+//		String message = "  Hello World  " +"!! ";
+//		
+//		System.out.println(message.endsWith("!!"));
+//		System.out.println(message.startsWith("!!"));
+//		System.out.println(message.length());
+//		System.out.println(message.indexOf('W'));
+//		System.out.println(message.indexOf('z'));
+//		System.out.println(message.replace("W", "J"));
+//		System.out.println(message.replace("Wo", "Ja"));
+//		System.out.println(message);
+//		System.out.println(message.toLowerCase());
+//		System.out.println(message.toUpperCase());
+//		System.out.println(message.trim());
+		
+		
+		System.out.println("Hello \"jasee\"");
+		System.out.println("C:\\Users\\Jasee\\OneDrive\\Documents");
+		System.out.println("C:\nUsers\\Jasee\\OneDrive\\Documents");
+	}
+}
