0338e0a9119fdf91101417815adb488eda68b4e0
jaseelan <93515798+jaseelan@users.noreply.github.com>
2021-12-14 16:49:43 +0530

    Add files via upload

---
+++ fingGene.java
@@ -0,0 +1,12 @@
+
+public class fingGene {
+	public static void main(String[] args) {
+		String s = "dukeprogramming";
+		String x = s.substring(3, 7);
+		int y = s.length();
+		int z = s.indexOf("program");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+}
