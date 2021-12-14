6d7d96045648dee7514760b4db141941649aeec0
jaseelan <93515798+jaseelan@users.noreply.github.com>
2021-12-14 16:47:54 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] number = new int[8];
+		number[0] = 1;
+		number[2] = 3;
+		number[6] = 9;
+		System.out.println(number);
+		System.out.println(Arrays.toString(number));
+
+	}
+
+}
