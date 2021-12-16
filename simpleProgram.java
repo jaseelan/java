1e71e0c520d8f7b5d5b0d05a48bc2dd596351b5a
jaseelan <93515798+jaseelan@users.noreply.github.com>
2021-12-16 18:26:26 +0530

    update

--- simpleProgram.java
+++ simpleProgram.java
@@ -1,24 +1,24 @@
 package simpleProgram;
 
+import java.awt.image.renderable.ContextualRenderedImageFactory;
 import java.util.Scanner;
 
 public class simpleProgram {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int total = 0;
-		int grade;
-		int average;
-		int counter = 0;
-
-		while (counter < 10) {
-			grade = input.nextInt();
-			total = total + grade;
-			counter++;
+		/*
+		 * Scanner input = new Scanner(System.in); int total = 0; int grade; int
+		 * average; int counter = 0;
+		 * 
+		 * while (counter < 10) { grade = input.nextInt(); total = total + grade;
+		 * counter++;
+		 * 
+		 * } average = total / 10; System.out.println("your average is " + average);
+		 */
+		for (int counter = 1; counter <= 10; counter+=2) {
+			System.out.println(counter);
 
 		}
-		average = total / 10;
-		System.out.println("your average is " + average);
 	}
 
 }
