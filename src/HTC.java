1963bf3d9f0151a2727e32c4e2bae868e8c2bc76
jaseelan <93515798+jaseelan@users.noreply.github.com>
2021-12-16 09:53:52 +0530

    method callig

---
+++ src/HTC.java
@@ -0,0 +1,6 @@
+
+public class HTC {
+public void simpleMessage() {
+	System.out.println("This is HTC class");
+}
+}
---
+++ src/KaliLinux.java
@@ -0,0 +1,7 @@
+
+public class KaliLinux {
+public static void main(String []args) {
+	HTC htcObject=new HTC(); //object create
+	htcObject.simpleMessage();
+}
+}
