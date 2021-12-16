996dafba81894a257c127435055711c4315a0879
jaseelan <93515798+jaseelan@users.noreply.github.com>
2021-12-16 09:14:55 +0530

    switch statement

---
+++ ScannerObject.java
@@ -0,0 +1,56 @@
+package timeConvation;
+
+import java.util.Scanner;
+
+public class ScannerObject {
+
+	public static void main(String[] args) {
+		/*
+		 * Scanner jasee = new Scanner(System.in); double firstNumber, secondNumber,
+		 * sum; String jaseelan = jasee.nextLine();
+		 * System.out.println(jasee.nextLine());
+		 * System.out.print("Enter the first number:"); firstNumber =
+		 * jasee.nextDouble(); System.out.print("second the first number:");
+		 * secondNumber = jasee.nextDouble(); sum = firstNumber + secondNumber;
+		 * System.out.println(sum);
+		 */
+
+		/*
+		 * int test = 6; if (test == 9) { System.out.println("yes"); } else {
+		 * System.out.println("no"); }
+		 */
+
+		/*
+		 * int Airtel, Dialog; Airtel = 399; Dialog = 495;
+		 * 
+		 * if (Airtel < 400 && Dialog < 500) {
+		 * System.out.println("You can buy data packge"); } else {
+		 * System.out.println("You can't buy data packge"); }
+		 */
+
+		/*
+		 * int age; age = 1; switch (age) { case 1: System.out.println("You can crawl");
+		 * break; case 2: System.out.println("You can talk"); break; case 3:
+		 * System.out.println("You can in toruble"); break; default:
+		 * System.out.println("out of switch"); }
+		 */
+		
+		int counter=0;
+		while(counter<10) {
+			System.out.println(counter);
+			counter++;			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+	}
+
+}
